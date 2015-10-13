d016a772863b332c7a2cdff61d77d1022eb0abb9
terterfred <fred545915853@hotmail.com>
2015-10-13 15:34:42 -0500

    no message

--- 02 Data Wrangling/SQL TO R.R
+++ 02 Data Wrangling/SQL TO R.R
@@ -2,6 +2,7 @@ require("jsonlite")
 require("RCurl")
 require(dplyr)
 require(lubridate)
+require(ggplot2)
 
 # Change the USER and PASS below to be your UTEid
 FF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from FAMA_FRENCH"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_tl23642', PASS='orcl_tl23642', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
@@ -10,6 +11,12 @@ GF <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?que
 
 GF<- GF %>% mutate(YEAR = year(parse_date_time(DATE_, "%m%d%y")))
 GF<- GF %>% mutate(MONTH = month(parse_date_time(DATE_, "%m%d%y")))
+FF<- FF %>% mutate(RF = RF/100)
+FF<- FF %>% mutate(MKT_RF= MKT_RF/100)
 
-FLJoin <- dplyr::full_join(GF, FF, by=c("YEAR","MONTH"))
+InJoin <- dplyr::inner_join(GF, FF, by=c("YEAR","MONTH"))
+retm <- as.numeric(as.character(unlist(InJoin[4])))
+InJoin <- InJoin %>% mutate(retm_rf = retm  - RF)
+InJoin <- InJoin %>% mutate(retm_mkt = retm  - MKT_RF)
 
+InJoin %>% group_by(ICDI) %>%filter(retm_mkt<=2)%>% summarise(avg = mean(retm_mkt)) %>% ggplot(aes(x=ICDI, y=avg)) + geom_point()
